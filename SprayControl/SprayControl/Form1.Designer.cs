b6186a4216cfe0cb4cfb83f3faae20ae995c3c0e
Nigel McDonald <myname_2@hotmail.com>
2021-05-24 18:13:54 +1000

    Main code dump
    includes visual studio project and exe

---
+++ SprayControl/SprayControl/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace SprayControl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.urlTextBox = new System.Windows.Forms.TextBox();
+            this.urlLabel = new System.Windows.Forms.Label();
+            this.urlButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(337, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 98);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "All Off";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 261);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(799, 189);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // clearButton
+            // 
+            this.clearButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.clearButton.Location = new System.Drawing.Point(712, 232);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 23);
+            this.clearButton.TabIndex = 2;
+            this.clearButton.Text = "Clear Log";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(171, 136);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(95, 65);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Toggle Sensor";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Location = new System.Drawing.Point(536, 136);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(95, 65);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Toggle Water";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button4.Location = new System.Drawing.Point(352, 149);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(94, 52);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Spray";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // urlTextBox
+            // 
+            this.urlTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.urlTextBox.Location = new System.Drawing.Point(38, 65);
+            this.urlTextBox.Name = "urlTextBox";
+            this.urlTextBox.Size = new System.Drawing.Size(269, 20);
+            this.urlTextBox.TabIndex = 6;
+            // 
+            // urlLabel
+            // 
+            this.urlLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.urlLabel.AutoSize = true;
+            this.urlLabel.Location = new System.Drawing.Point(35, 49);
+            this.urlLabel.Name = "urlLabel";
+            this.urlLabel.Size = new System.Drawing.Size(77, 13);
+            this.urlLabel.TabIndex = 7;
+            this.urlLabel.Text = "Enter API URL";
+            this.urlLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // urlButton
+            // 
+            this.urlButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.urlButton.Location = new System.Drawing.Point(38, 91);
+            this.urlButton.Name = "urlButton";
+            this.urlButton.Size = new System.Drawing.Size(75, 23);
+            this.urlButton.TabIndex = 8;
+            this.urlButton.Text = "Save";
+            this.urlButton.UseVisualStyleBackColor = true;
+            this.urlButton.Click += new System.EventHandler(this.urlButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(799, 450);
+            this.Controls.Add(this.urlButton);
+            this.Controls.Add(this.urlLabel);
+            this.Controls.Add(this.urlTextBox);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Auto Spray System Controller";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox urlTextBox;
+        private System.Windows.Forms.Label urlLabel;
+        private System.Windows.Forms.Button urlButton;
+    }
+}
+
---
+++ SprayControl/SprayControl/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+
+namespace SprayControl
+{
+    public partial class Form1 : Form
+    {
+        private static string apiUrl = "https://api.particle.io/v1/devices/e00fce68cb4c6f6733534412/SpraySystem?access_token=292b0e92e972aa309c7658b96530f868de081a03";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void GetRequest(string url)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                using (HttpResponseMessage responce = await client.GetAsync(url))
+                {
+                    using (HttpContent content = responce.Content)
+                    {
+                        string myContent = await content.ReadAsStringAsync();    
+                        richTextBox1.Text = myContent + "\n" + richTextBox1.Text;
+                    }
+                }
+            }
+        }
+
+        private async void PostRequest(string url, string value)
+        {
+            IEnumerable<KeyValuePair<string, string>> kvp = new List<KeyValuePair<string, string>>()
+            {
+                new KeyValuePair<string, string>(value, value)
+            };
+            HttpContent cont = new FormUrlEncodedContent(kvp);
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    using (HttpResponseMessage responce = await client.PostAsync(url, cont))
+                    {
+                        using (HttpContent content = responce.Content)
+                        {
+                            string myContent = await content.ReadAsStringAsync();
+                            ProcessResponce(myContent);                            
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    richTextBox1.Text = e + "\n" + richTextBox1.Text;
+                }
+                
+            }
+        }
+
+        private void ProcessResponce(String value)
+        {
+            if (value.Contains("99"))
+            {
+                button4.ResetBackColor();
+                button4.UseVisualStyleBackColor = true;
+                richTextBox1.Text = "Sending Command: Spray Trigger" + "\n" + "Recieved Response: Spray Triggerd." + "\n" + richTextBox1.Text;
+            }
+            else if (value.Contains("100"))
+            {
+                button1.ResetBackColor();
+                button1.UseVisualStyleBackColor = true;
+                richTextBox1.Text = "Sending Command: Enable Sensor" + "\n" + "Recieved Response: Sensing On!" + "\n" + richTextBox1.Text;
+            }
+            else if (value.Contains("101"))
+            {
+                button2.ResetBackColor();
+                button2.UseVisualStyleBackColor = true;
+                richTextBox1.Text = "Sending Command: Disable Sensor" + "\n" + "Recieved Response: Sensing Off!" + "\n" + richTextBox1.Text;
+            }
+            else if (value.Contains("102"))
+            {
+                button1.ResetBackColor();
+                button1.UseVisualStyleBackColor = true;
+                richTextBox1.Text = "Sending Command: Enable Water" + "\n" + "Recieved Response: Water On!" + "\n" + richTextBox1.Text;
+            }
+            else if (value.Contains("103"))
+            {
+                button3.ResetBackColor();
+                button3.UseVisualStyleBackColor = true;
+                richTextBox1.Text = "Sending Command: Disable Water" + "\n" + "Recieved Response: Water Off!" + "\n" + richTextBox1.Text;
+            }
+            else if (value.Contains("104"))
+            {
+                richTextBox1.Text = "Sending Command: All Stop!" + "\n" + "Recieved Response: All Systems Disabled" + "\n" + richTextBox1.Text;
+            }
+
+        }
+
+        //all off button
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.BackColor = Color.Salmon;
+            PostRequest(apiUrl, "off");
+        }
+
+        // this button clears the text log
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+        }
+
+        // sensor button
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2.BackColor = Color.LightYellow;
+            PostRequest(apiUrl, "sensor");
+        }
+
+        //spray button
+        private void button4_Click(object sender, EventArgs e)
+        {
+            button4.BackColor = Color.LightBlue;
+            PostRequest(apiUrl, "spray");
+        }
+
+        //water button
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button3.BackColor = Color.LightBlue;
+            PostRequest(apiUrl, "water");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void urlButton_Click(object sender, EventArgs e)
+        {
+            apiUrl = urlTextBox.Text;
+            richTextBox1.Text = "URL: " + apiUrl + "\n" + "SAVED" + "\n" + richTextBox1.Text;
+            urlTextBox.Clear();
+        }
+    }
+}
